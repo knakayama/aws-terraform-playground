a040a97f4f32464ee07278525bccebe52d7a2b8d
knakayama <knakayama.sh@gmail.com>
2016-07-10 15:57:24 +0900

    update

---
+++ simple/network.tf
@@ -0,0 +1,53 @@
+resource "aws_vpc" "vpc" {
+  cidr_block           = "${var.vpc_cidr}"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+resource "aws_internet_gateway" "public" {
+  vpc_id = "${aws_vpc.vpc.id}"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = "${aws_vpc.vpc.id}"
+  cidr_block              = "${cidrsubnet(var.vpc_cidr, 8, 1)}"
+  availability_zone       = "${data.aws_availability_zones.az.names[1]}"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.public.id}"
+  }
+}
+
+resource "aws_route_table_association" "public" {
+  subnet_id      = "${aws_subnet.public.id}"
+  route_table_id = "${aws_route_table.public.id}"
+}
+
+resource "aws_network_acl" "acl" {
+  vpc_id     = "${aws_vpc.vpc.id}"
+  subnet_ids = ["${aws_subnet.public.id}"]
+
+  ingress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = "-1"
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+}
