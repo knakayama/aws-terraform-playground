df030d4941f83c1ec88dd1982647c621c242034c
knakayama <knakayama.sh@gmail.com>
2016-07-16 20:59:11 +0900

    update

---
+++ simple/elb-simple/variables.tf
@@ -0,0 +1,59 @@
+variable "name" {
+  default = "simple"
+}
+
+variable "region" {
+  default = "ap-northeast-1"
+}
+
+variable "vpc_cidr" {
+  default = "172.16.0.0/16"
+}
+
+variable "web_instance_type" {
+  default = "c3.large"
+}
+
+variable "spot_price" {
+  default = "0.1"
+}
+
+variable "wait_for_fulfillment" {
+  default = true
+}
+
+variable "spot_type" {
+  default = "one-time"
+}
+
+data "aws_availability_zones" "az" {}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "block-device-mapping.volume-type"
+    values = ["gp2"]
+  }
+}
