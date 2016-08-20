c15213493aeff8671ffa44bbbca76e9ded241477
knakayama <knakayama.sh@gmail.com>
2016-08-20 20:51:06 +0900

    update

---
+++ network/public1-private1/security_group.tf
@@ -0,0 +1,60 @@
+resource "aws_security_group" "web" {
+  name        = "${var.name}-web-sg"
+  vpc_id      = "${aws_vpc.vpc.id}"
+  description = "${var.name} web sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_security_group" "db" {
+  name        = "${var.name}-db-sg"
+  vpc_id      = "${aws_vpc.vpc.id}"
+  description = "${var.name} db sg"
+
+  ingress {
+    from_port       = 1521
+    to_port         = 1521
+    protocol        = "tcp"
+    security_groups = ["${aws_security_group.web.id}"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
