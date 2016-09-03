57710438af5c18e6eb5b13d60350752ef81d07c7
knakayama <knakayama.sh@gmail.com>
2016-09-03 18:12:49 +0900

    Update

--- rds/rds-network/outputs.tf
+++ rds/rds-network/outputs.tf
@@ -1,3 +1,7 @@
 output "web_public_ip" {
   value = "${aws_instance.web.public_ip}"
 }
+
+output "s3_arn" {
+  value = "${aws_s3_bucket.s3.arn}"
+}
--- rds/rds-network/region/tokyo/outputs.tf
+++ rds/rds-network/region/tokyo/outputs.tf
@@ -1,3 +1,7 @@
 output "web_public_ip" {
   value = "${module.main.web_public_ip}"
 }
+
+output "s3_arn" {
+  value = "${module.main.s3_arn}"
+}
