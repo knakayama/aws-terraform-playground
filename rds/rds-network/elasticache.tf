664c3a849ce482facbb439004221da9a1353ec6a
knakayama <knakayama.sh@gmail.com>
2016-09-03 18:06:16 +0900

    Add elasticache pg

--- rds/rds-network/elasticache.tf
+++ rds/rds-network/elasticache.tf
@@ -3,3 +3,8 @@ resource "aws_elasticache_subnet_group" "redis" {
   subnet_ids  = ["${aws_subnet.private.*.id}"]
   description = "${replace(var.name, "_", " ")}"
 }
+
+resource "aws_elasticache_parameter_group" "default" {
+  name   = "${var.name}"
+  family = "redis2.8"
+}
