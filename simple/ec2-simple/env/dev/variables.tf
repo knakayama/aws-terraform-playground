6f732817f1715e2651aabf1e889d247802272d5e
knakayama <knakayama.sh@gmail.com>
2016-09-14 18:17:44 +0900

    update

--- simple/ec2-simple/env/dev/variables.tf
+++ simple/ec2-simple/env/dev/variables.tf
@@ -1,5 +1,5 @@
 variable "name" {
-  default = "simple-prd"
+  default = "simple-dev"
 }
 
 variable "region" {
--- simple/ec2-simple/env/stg/variables.tf
+++ simple/ec2-simple/env/stg/variables.tf
@@ -1,5 +1,5 @@
 variable "name" {
-  default = "simple-prd"
+  default = "simple-stg"
 }
 
 variable "region" {
