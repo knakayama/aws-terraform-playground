76bdde9967cb12550121d66379a62117b1f57be2
knakayama <knakayama.sh@gmail.com>
2016-09-24 11:06:40 +0900

    Add asg-simple

---
+++ simple/asg-simple/variables.tf
@@ -0,0 +1,52 @@
+variable "name" {
+  default = "alb-simple"
+}
+
+variable "region" {
+  default = "ap-northeast-1"
+}
+
+variable "vpc_cidr" {
+  default = "172.16.0.0/16"
+}
+
+variable "asg_config" {
+  default = {
+    instance_type = "t2.nano"
+    desired       = 2
+    max           = 2
+    min           = 2
+  }
+}
+
+data "aws_availability_zones" "az" {}
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "name"
+    values = ["amzn-ami-hvm-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "block-device-mapping.volume-type"
+    values = ["gp2"]
+  }
+}
