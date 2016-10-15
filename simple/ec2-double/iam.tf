0635719d5c45a2b8b2bd20caead2e48f58f67752
knakayama <knakayama.sh@gmail.com>
2016-10-15 22:25:52 +0900

    update

--- simple/ec2-double/iam.tf
+++ simple/ec2-double/iam.tf
@@ -9,12 +9,6 @@ resource "aws_iam_policy_attachment" "cloudwatch_full_access" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
 }
 
-resource "aws_iam_policy_attachment" "s3_read_only_access" {
-  name       = "S3WatchReadOnlyAccess"
-  roles      = ["${aws_iam_role.ec2_role.name}"]
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
-}
-
 resource "aws_iam_instance_profile" "instance_profile" {
   name  = "${var.name}-instance-profile"
   roles = ["${aws_iam_role.ec2_role.name}"]
