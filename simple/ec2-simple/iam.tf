415a84146fb2b8b5717e10b335415bd654e1c760
knakayama <knakayama.sh@gmail.com>
2017-01-10 19:56:34 +0900

    Update

--- simple/ec2-simple/iam.tf
+++ simple/ec2-simple/iam.tf
@@ -1,6 +1,19 @@
+data "aws_iam_policy_document" "ec2" {
+  statement {
+    sid     = "EC2AssumeRole"
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals = {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
+  }
+}
+
 resource "aws_iam_role" "ec2_role" {
   name               = "${var.name}-ec2-role"
-  assume_role_policy = "${file("${path.module}/policies/ec2_assume_role_policy.json")}"
+  assume_role_policy = "${data.aws_iam_policy_document.ec2.json}"
 }
 
 resource "aws_iam_policy_attachment" "cloudwatch_full_access" {
@@ -9,10 +22,10 @@ resource "aws_iam_policy_attachment" "cloudwatch_full_access" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
 }
 
-resource "aws_iam_policy_attachment" "s3_read_only_access" {
-  name       = "S3WatchReadOnlyAccess"
+resource "aws_iam_policy_attachment" "s3_full_access" {
+  name       = "S3FullAccess"
   roles      = ["${aws_iam_role.ec2_role.name}"]
-  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
 }
 
 resource "aws_iam_instance_profile" "instance_profile" {
