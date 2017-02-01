a5593668f347de44e9a966aa0c8086f460517a05
knakayama <knakayama.sh@gmail.com>
2017-02-01 12:29:06 +0900

    Add s3 full access

--- simple/asg-simple/iam.tf
+++ simple/asg-simple/iam.tf
@@ -9,6 +9,12 @@ resource "aws_iam_policy_attachment" "cloudwatch_full_access" {
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchFullAccess"
 }
 
+resource "aws_iam_policy_attachment" "s3_full_access" {
+  name       = "S3FullAccess"
+  roles      = ["${aws_iam_role.ec2_role.name}"]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
 resource "aws_iam_instance_profile" "instance_profile" {
   name  = "${var.name}-instance-profile"
   roles = ["${aws_iam_role.ec2_role.name}"]
