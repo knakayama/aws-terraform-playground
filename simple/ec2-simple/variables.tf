bb0d473474b084c3514806505a08e94c5d434957
knakayama <knakayama.sh@gmail.com>
2017-04-07 16:42:40 +0900

    update

--- simple/ec2-simple/variables.tf
+++ simple/ec2-simple/variables.tf
@@ -1,9 +1,22 @@
-variable "name" {}
+variable "name" {
+  default = "simple-dev"
+}
 
-variable "vpc_cidr" {}
+variable "region" {
+  default = "ap-northeast-1"
+}
+
+variable "vpc_cidr" {
+  default = "172.16.0.0/16"
+}
 
 variable "spot_config" {
-  type = "map"
+  default = {
+    instance_type        = "c3.large"
+    price                = "0.15"
+    wait_for_fulfillment = true
+    type                 = "one-time"
+  }
 }
 
 data "aws_availability_zones" "az" {}
